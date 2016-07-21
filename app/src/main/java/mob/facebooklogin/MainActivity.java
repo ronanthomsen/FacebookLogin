8dd5ba17e940ef59ed97333bff29f08b407f2a5c
ronanthomsen <ronanabdo@gmail.com>
2016-07-21 16:05:40 -0300

    Android App to make FaceBook Login

---
+++ app/src/main/java/mob/facebooklogin/MainActivity.java
@@ -0,0 +1,55 @@
+package mob.facebooklogin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i("FACEBOOK", "Sucesso");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("FACEBOOK", "Cancelado");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.i("FACEBOOK", "Erro" + error.toString());
+            }
+
+            });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
